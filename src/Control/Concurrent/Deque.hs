384b9fc877244da50bbb7840c7349996d38f105a
Edward Kmett <ekmett@gmail.com>
2013-09-03 21:15:40 -0400

    started doctesting. fixed fromList and fromListN

--- src/Control/Concurrent/Deque.hs
+++ src/Control/Concurrent/Deque.hs
@@ -16,7 +16,7 @@ module Control.Concurrent.Deque
   , null
   , size
   -- * Local Operations
-  , push
+  , push, pushMany
   , pop
   -- * Work-Stealing
   , steal
@@ -25,6 +25,7 @@ module Control.Concurrent.Deque
 import Control.Exception (evaluate)
 import Data.Atomics (storeLoadBarrier, writeBarrier, loadLoadBarrier)
 import Data.Atomics.Counter.Reference
+import Data.Foldable
 import Data.IORef
 import Data.Vector.Array
 import qualified Data.Vector.Generic as V
@@ -32,7 +33,14 @@ import qualified Data.Vector.Generic.Mutable as MV
 import GHC.Prim (RealWorld)
 import Prelude hiding (null)
 
+-- $setup
+-- >>> :set -XScopedTypeVariables
+
 -- | A Chase-Lev circular work-stealing deque
+--
+-- A work-stealing deque is perhaps more properly referred to as a concurrent steque,
+-- where the dequeue-only 'steal' side can be used concurrently, but the \"local\" side may
+-- that provides 'push' and 'pop' can only be used from a single owner thread.
 data Deque a = Deque
   { _bottom :: {-# UNPACK #-} !AtomicCounter
   , _top    :: {-# UNPACK #-} !AtomicCounter
@@ -40,6 +48,10 @@ data Deque a = Deque
   }
 
 -- | Create a new 'empty' 'Deque'.
+--
+-- >>> q :: Deque Int <- empty
+-- >>> null q
+-- True
 empty :: Arrayed a => IO (Deque a)
 empty = do
   v <- MV.new 32
@@ -48,23 +60,37 @@ empty = do
   ref <- newIORef v
   return (Deque bot top ref)
 
+-- | Generate a work-stealing 'Deque' of elements from a list.
+--
+-- >>> q <- fromList [1,2,3,4 :: Int]
+-- >>> pop q
+-- Just 1
+-- >>> pop q
+-- Just 2
+-- >>> pop q
+-- Just 3
+-- >>> pop q
+-- Just 4
+-- >>> pop q
+-- Nothing
 fromList :: forall a. Arrayed a => [a] -> IO (Deque a)
 fromList as = do
-  v <- V.unsafeThaw (V.fromList as :: Array a)
-  bot <- newCounter (MV.length v - 1)
+  v <- V.unsafeThaw (V.reverse (V.fromList as :: Array a)) -- TODO: pad this out to an initial 32 entries?
+  bot <- newCounter (MV.length v)
   top <- newCounter 0
   ref <- newIORef v
   return (Deque bot top ref)
 
+-- | Generate a work-stealing 'Deque' of elements from a list of known length.
 fromListN :: forall a. Arrayed a => Int -> [a] -> IO (Deque a)
 fromListN n as = do
-  v <- V.unsafeThaw (V.fromListN n as :: Array a)
-  bot <- newCounter (MV.length v - 1)
+  v <- V.unsafeThaw (V.reverse (V.fromListN n as :: Array a))
+  bot <- newCounter (MV.length v)
   top <- newCounter 0
   ref <- newIORef v
   return (Deque bot top ref)
 
--- | Return 'True' if the 'Deque' is definitely empty.
+-- | @null@ returns 'True' if the 'Deque' is definitely empty.
 null :: Deque a -> IO Bool
 null (Deque bot top _) = do
   b <- readCounter bot
@@ -73,13 +99,13 @@ null (Deque bot top _) = do
   return (sz <= 0)
 
 -- | Compute a lower and upper bound on the number of elements left in the 'Deque'.
-size :: Deque a -> IO (Int,Int)
+size :: Deque a -> IO (Int, Int)
 size (Deque bot top _) = do
   b1 <- readCounter bot
   t  <- readCounter top
   b2 <- readCounter bot
-  let size1 = b1 - t
-      size2 = b2 - t
+  let size1 = b1 - t -- always the lower bound on x86, due to lack of load reordering
+      size2 = b2 - t -- always the upper bound on x86, due to lack of load reordering
   return (min size1 size2, max size1 size2)
 
 -- * Queue Operations
@@ -113,8 +139,19 @@ push obj (Deque bottom top array) = do
   writeCounter bottom (b+1)
   return ()
 
--- | This is the steal operation.  Multiple threads may concurrently
--- attempt steals from the same thread.
+-- TODO: consolidate the writes behind one barrier!
+pushMany :: (Foldable f, Arrayed a) => f a -> Deque a -> IO ()
+pushMany objs d = forM_ objs $ \a -> push a d
+{-# INLINE pushMany #-}
+
+-- | This is the work-stealing dequeue operation.
+--
+-- Multiple threads may concurrently attempt to 'steal' from the same thread.
+--
+-- It may return 'Nothing' during a lost race with another concurrent reader,
+-- so failing to read is /not/ an indication that the 'Deque' is empty!
+--
+-- However, at least one of the concurrently stealing threads will succeed.
 steal :: Arrayed a => Deque a -> IO (Maybe a)
 steal (Deque bottom top array) = do
   -- NB. these loads must be ordered, otherwise there is a race
@@ -131,7 +168,8 @@ steal (Deque bottom top array) = do
     return $! if b' then Just a
                     else Nothing -- Someone beat us, abort
 
--- | Locally pop the deque.
+-- | Locally pop the 'Deque'. This is not a thread safe operation, and should
+-- only be invoked on from the thread that \"owns\" the 'Deque'.
 pop :: Arrayed a => Deque a -> IO (Maybe a)
 pop (Deque bottom top array) = do
   b0  <- readCounter bottom
@@ -162,13 +200,13 @@ pop (Deque bottom top array) = do
 
 -- My own forM for numeric ranges (not requiring deforestation optimizations).
 -- Inclusive start, exclusive end.
-for_ :: Monad m => Int -> Int -> (Int -> m ()) -> m ()
-for_ start end _ | start > end = error "for_: start is greater than end"
-for_ start end fn = loop start
+forN_ :: Monad m => Int -> Int -> (Int -> m ()) -> m ()
+forN_ start end _ | start > end = error "for_: start is greater than end"
+forN_ start end fn = loop start
   where
    loop !i | i == end  = return ()
            | otherwise = do fn i; loop (i+1)
-{-# INLINE for_ #-}
+{-# INLINE forN_ #-}
 
 -- * Circular array routines:
 
@@ -177,7 +215,7 @@ growCirc :: Arrayed a => Int -> Int -> MArray RealWorld a -> IO (MArray RealWorl
 growCirc s e old = do
   let len = MV.length old
   new <- MV.unsafeNew (len + len)
-  for_ s e $ \i -> do
+  forN_ s e $ \i -> do
     x <- getCirc old i
     _ <- evaluate x
     putCirc new i x
@@ -191,4 +229,3 @@ getCirc arr ind = MV.unsafeRead arr (ind `mod` MV.length arr)
 putCirc :: Arrayed a => MArray RealWorld a -> Int -> a -> IO ()
 putCirc arr ind x = MV.unsafeWrite arr (ind `mod` MV.length arr) x
 {-# INLINE putCirc #-}
-
