d42b79efd842e5cba28b2caf41d4f066167556a5
Edward Kmett <ekmett@gmail.com>
2013-09-05 12:49:30 -0400

    Added insert and modify. More inlining

--- src/Data/Vector/Bloom.hs
+++ src/Data/Vector/Bloom.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE RankNTypes #-}
 module Data.Vector.Bloom
   ( Bloom(Bloom)
   , width
   , bloom
   , elem
+  , modify
+  , insert
   -- * Freezing/Thawing
   , freeze, thaw
   , unsafeFreeze, unsafeThaw
@@ -26,6 +29,8 @@ import Data.Vector.Bloom.Util
 import Data.Word
 import Prelude hiding (elem)
 
+-- TODO: switch to a hash that we can persist to disk cross-platform!
+
 data Bloom = Bloom
   { _hashes  :: {-# UNPACK #-} !Int -- number of hash functions to use
   , _bits    :: !(U.Vector Word64)  -- data
@@ -37,33 +42,49 @@ bloom k m fa = runST $ do
   let mb = MB.MBloom k v
   F.forM_ fa $ \a -> MB.insert a mb
   freeze mb
+{-# INLINE bloom #-}
 
 elem :: Hashable a => a -> Bloom -> Bool
 elem a (Bloom h v) = all hit (hashes h a) where
   !m = U.length v
   hit i = testBit (U.unsafeIndex v (mod (unsafeShiftR i 6) m)) (i .&. 63)
+{-# INLINE elem #-}
+
+insert :: Hashable a => a -> Bloom -> Bloom
+insert a b = modify (MB.insert a) b
+{-# INLINE insert #-}
+
+modify :: (forall s. MBloom s -> ST s ()) -> Bloom -> Bloom
+modify f (Bloom a v) = Bloom a (U.modify (f . MBloom a) v)
+{-# INLINE modify #-}
 
 width :: Bloom -> Int
 width (Bloom _ w) = unsafeShiftL (U.length w) 6
+{-# INLINE width #-}
 
 instance Semigroup Bloom where
   Bloom k1 v1 <> Bloom k2 v2 =  Bloom (min k1 k2) v3
     where m1 = U.length v1
           m2 = U.length v2
           v3 = U.generate (lcm m1 m2) $ \i -> U.unsafeIndex v1 (mod i m1) .|. U.unsafeIndex v2 (mod i m2)
+  {-# INLINE (<>) #-}
 
 -- | /O(m)/
 freeze :: PrimMonad m => MBloom (PrimState m) -> m Bloom
 freeze (MBloom k bs) = Bloom k `liftM` U.freeze bs
+{-# INLINE freeze #-}
 
 -- | /O(m)/
 thaw :: PrimMonad m => Bloom -> m (MBloom (PrimState m))
 thaw (Bloom k bs) = MBloom k `liftM` U.thaw bs
+{-# INLINE thaw #-}
 
 -- | /O(1)/
 unsafeFreeze :: PrimMonad m => MBloom (PrimState m) -> m Bloom
 unsafeFreeze (MBloom k bs) = Bloom k `liftM` U.unsafeFreeze bs
+{-# INLINE unsafeFreeze #-}
 
 -- | /O(1)/
 unsafeThaw :: PrimMonad m => Bloom -> m (MBloom (PrimState m))
 unsafeThaw (Bloom k bs) = MBloom k `liftM` U.unsafeThaw bs
+{-# INLINE unsafeThaw #-}
