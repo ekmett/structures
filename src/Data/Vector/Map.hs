ecd6ff8c90cd0e3cb0b3a741b885acea14d0ea5d
Edward Kmett <ekmett@gmail.com>
2013-09-09 16:57:14 -0400

    crank up the blooms

--- src/Data/Vector/Map.hs
+++ src/Data/Vector/Map.hs
@@ -90,7 +90,7 @@ baseRate = 0.001
 
 blooming :: (Hashable k, Arrayed k) => Array k -> Maybe B.Bloom
 blooming ks
-  | n < 25000 = Nothing
+  | n < 10 = Nothing
   | m <- optimalWidth n $ baseRate / log (fromIntegral n)
   , k <- optimalHashes n m = Just $ runST $ do
     mb <- MB.mbloom k m
