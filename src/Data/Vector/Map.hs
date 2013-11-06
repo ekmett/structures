c3f83540710f7ade2d7f59bc93ff4db71a3d4154
Edward Kmett <ekmett@gmail.com>
2013-11-06 10:11:39 -0500

    Make the simplest modification to Data.Vector.Map to use the Overmars representaton.
    * Currently ~8x slower than Data.Map

--- src/Data/Vector/Map.hs
+++ src/Data/Vector/Map.hs
@@ -22,30 +22,15 @@
 --
 -- This module provides a 'Vector'-based 'Map' that is loosely based on the
 -- Cache Oblivious Lookahead Array (COLA) by Bender et al. from
--- <http://supertech.csail.mit.edu/papers/sbtree.pdf "Cache-Oblivious Streaming B-Trees">.
+-- <http://supertech.csail.mit.edu/papers/sbtree.pdf "Cache-Oblivious Streaming B-Trees">,
+-- but with inserts deamortized using a technique from Overmars and van Leeuwen.
 --
 -- Currently this 'Map' is implemented in an insert-only fashion. Deletions are left to future work
 -- or to another derived structure in case they prove expensive.
 --
--- Unlike the COLA, this version merely provides amortized complexity bounds as this permits us to
--- provide a fully functional API. However, even those asymptotics are only guaranteed if you do not
--- modify the \"old\" versions of the 'Map'. If you do, then while correctness is preserved, the
--- asymptotic analysis becomes inaccurate.
---
--- Reading from \"old\" versions of the 'Map' does not affect the asymptotic analysis and is fine.
---
--- Fractional cascading was originally replaced with the use of a hierarchical bloom filter per level containing
--- the elements for that level, with the false positive rate tuned to balance the lookup cost against
--- the costs of the cache misses for a false positive at that depth. This avoids the need to collect
--- forwarding pointers from the next level, reducing pressure on the cache dramatically, while providing
--- the same asymptotic complexity.
---
--- With either of these two techniques when used ephemerally, this 'Map' had asymptotic performance equal to that
--- of a B-Tree tuned to the parameters of your caches with requiring such parameter tuning.
---
--- However, the constants were still bad enough that the naive /O(log^2 n)/ version of the COLA actually wins
--- at lookups in benchmarks at the scale this data structure is interesting, say around a few million entries,
--- by a factor of 10x! Consequently, we're currently not even Bloom filtering.
+-- Currently, we also do not use fractional cascading, as it affects the constant factors badly enough
+-- to not pay for itself at the scales we are interested in. The naive /O(log^2 n)/ lookup 
+-- consistently outperforms the alternative.
 --
 -- Compared to the venerable @Data.Map@, this data structure currently consumes more memory, but it
 -- provides a more limited palette of operations with different asymptotics (~10x faster inserts at a million entries)
@@ -54,10 +39,6 @@
 -- /NB:/ when used with boxed data this structure may hold onto references to old versions
 -- of things for many updates to come until sufficient operations have happened to merge them out
 -- of the COLA.
---
--- TODO: track actual percentage of occupancy for each vector compared to the source vector it was based on.
--- This would permit 'split' and other operations that trim a 'Map' to properly reason about space usage by
--- borrowing the 1/3rd occupancy rule from a Stratified Doubling Array.
 -----------------------------------------------------------------------------
 module Data.Vector.Map
   ( Map(..)
@@ -67,7 +48,6 @@ module Data.Vector.Map
   , lookup
   , insert
   , fromList
-  , shape
   ) where
 
 import Data.Bits
@@ -78,78 +58,88 @@ import qualified Data.Vector.Fusion.Stream.Monadic as Stream
 import Data.Vector.Fusion.Util
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Map.Fusion as Fusion
+import Data.Vector.Slow as Slow
 import Prelude hiding (null, lookup)
 
 #define BOUNDS_CHECK(f) (Ck.f __FILE__ __LINE__ Ck.Bounds)
 
 -- | This Map is implemented as an insert-only Cache Oblivious Lookahead Array (COLA) with amortized complexity bounds
--- that are equal to those of a B-Tree when it is used ephemerally, using Bloom filters to replace the fractional
--- cascade.
-data Map k v
-  = Nil
-  | One !k v !(Map k v)
-  | Map !Int !(Array k) !(Array v) !(Map k v)
+-- that are equal to those of a B-Tree, except for an extra log factor slowdown on lookups due to the lack of fractional
+-- cascading.
 
-deriving instance (Show (Arr v v), Show (Arr k k), Show k, Show v) => Show (Map k v)
-deriving instance (Read (Arr v v), Read (Arr k k), Read k, Read v) => Read (Map k v)
+data Chunk k a = Chunk !(Array k) !(Array a)
+
+data Map k a
+  = M0
+  | M1 !(Array k) !(Array a)
+  | M2 !(Array k) !(Array a)
+       !(Array k) !(Array a) !(Partial (Chunk k a)) !(Map k a)
+  | M3 !(Array k) !(Array a)
+       !(Array k) !(Array a)
+       !(Array k) !(Array a) !(Partial (Chunk k a)) !(Map k a)
+
+deriving instance (Show (Arr v v), Show (Arr k k)) => Show (Map k v)
+deriving instance (Show (Arr v v), Show (Arr k k)) => Show (Chunk k v)
+
 
 -- | /O(1)/. Identify if a 'Map' is the 'empty' 'Map'.
 null :: Map k v -> Bool
-null Nil = True
-null _   = False
+null M0 = True
+null _  = False
 {-# INLINE null #-}
 
 -- | /O(1)/ The 'empty' 'Map'.
 empty :: Map k v
-empty = Nil
+empty = M0
 {-# INLINE empty #-}
 
 -- | /O(1)/ Construct a 'Map' from a single key/value pair.
-singleton :: Arrayed v => k -> v -> Map k v
-singleton k v = v `vseq` One k v Nil
+singleton :: (Arrayed k, Arrayed v) => k -> v -> Map k v
+singleton k v = M1 (G.singleton k) (G.singleton v)
 {-# INLINE singleton #-}
 
--- | /O(log^2 N)/ persistently amortized, /O(N)/ worst case. Lookup an element.
+-- | /O(log^2 N)/ worst-case. Lookup an element.
 lookup :: (Ord k, Arrayed k, Arrayed v) => k -> Map k v -> Maybe v
 lookup !k m0 = go m0 where
   {-# INLINE go #-}
-  go Nil = Nothing
-  go (One i a m)
-    | k == i    = Just a
-    | otherwise = go m
-  go (Map n ks vs m)
-    | j <- search (\i -> ks G.! i >= k) 0 (n-1)
-    , ks G.! j == k = Just $ vs G.! j
-    | otherwise = go m
+  go M0 = Nothing
+  go (M1 ka va)                 = lookup1 k ka va Nothing
+  go (M2 ka va kb vb _ m)       = lookup1 k ka va $ lookup1 k kb vb $ go m
+  go (M3 ka va kb vb kc vc _ m) = lookup1 k ka va $ lookup1 k kb vb $ lookup1 k kc vc $ go m
 {-# INLINE lookup #-}
 
-threshold :: Int -> Int -> Bool
-threshold n1 n2 = n1 > unsafeShiftR n2 1
-{-# INLINE threshold #-}
+lookup1 :: (Ord k, Arrayed k, Arrayed v) => k -> Array k -> Array v -> Maybe v -> Maybe v
+lookup1 k ks vs r
+  | j <- search (\i -> ks G.! i >= k) 0 (G.length ks - 1)
+  , ks G.! j == k = Just $ vs G.! j
+  | otherwise = r
+{-# INLINE lookup1 #-}
 
--- force a value as much as it would be forced by inserting it into an Array
-vseq :: forall a b. Arrayed a => a -> b -> b
-vseq a b = G.elemseq (undefined :: Array a) a b
-{-# INLINE vseq #-}
-
--- | O((log N)\/B) ephemerally amortized loads for each cache, O(N\/B) worst case. Insert an element.
+-- | O((log N)\/B) worst-case loads for each cache. Insert an element.
 insert :: (Ord k, Arrayed k, Arrayed v) => k -> v -> Map k v -> Map k v
-insert !k v (Map n1 ks1 vs1 (Map n2 ks2 vs2 m))
-  | threshold n1 n2 = insert2 k v ks1 vs1 ks2 vs2 m
-insert !ka va (One kb vb (One kc vc m)) = case G.unstream $ Fusion.insert ka va rest of
-    V_Pair n ks vs -> Map n ks vs m
-  where
-    rest = case compare kb kc of
-      LT -> Stream.fromListN 2 [(kb,vb),(kc,vc)]
-      EQ -> Stream.fromListN 1 [(kb,vb)]
-      GT -> Stream.fromListN 2 [(kc,vc),(kb,vb)]
-insert k v m = v `vseq` One k v m
-{-# INLINABLE insert #-}
+insert k0 v0 s0 = go (G.singleton k0) (G.singleton v0) s0 where
+  go ka a M0                    = M1 ka a
+  go ka a (M1 kb b)             = M2 ka a kb b (merge ka a kb b) M0
+  go ka a (M2 kb b kc c mbc xs) = M3 ka a kb b kc c (step mbc) (steps xs)
+  go ka a (M3 kb b _ _ _ _ mcd xs) = case mcd of
+    Stop (Chunk kcd cd) -> M2 ka a kb b (merge ka a kb b) (go kcd cd xs)
+    _       -> error "insert: stop Step"
 
-insert2 :: (Ord k, Arrayed k, Arrayed v) => k -> v -> Array k -> Array v -> Array k -> Array v -> Map k v -> Map k v
-insert2 k v ks1 vs1 ks2 vs2 m = case G.unstream $ Fusion.insert k v (zips ks1 vs1) `Fusion.merge` zips ks2 vs2 of
-  V_Pair n ks3 vs3 -> Map n ks3 vs3 m
-{-# INLINE insert2 #-}
+  steps (M2 kx x ky y mxy xs)   = M2 kx x ky y (step mxy) (steps xs)
+  steps (M3 kx x ky y kz z myz xs) = M3 kx x ky y kz z (step myz) (steps xs)
+  steps m = m
+{-# INLINE insert #-}
+
+step :: Partial a -> Partial a
+step (Stop _)  = error "insert: step Stop"
+step (Step m) = m
+{-# INLINE step #-}
+
+merge :: (Ord k, Arrayed k, Arrayed a) => Array k -> Array a -> Array k -> Array a -> Partial (Chunk k a)
+merge km m kn n = step $ walkST $ do
+  V_Pair _ ks vs <- Slow.unstreamM $ Slow.streamST $ Fusion.merge (zips km m) (zips kn n)
+  return $ Chunk ks vs
+{-# INLINE merge #-}
 
 fromList :: (Ord k, Arrayed k, Arrayed v) => [(k,v)] -> Map k v
 fromList xs = List.foldl' (\m (k,v) -> insert k v m) empty xs
@@ -171,10 +161,3 @@ search p = go where
 zips :: (G.Vector v a, G.Vector u b) => v a -> u b -> Stream Id (a, b)
 zips va ub = Stream.zip (G.stream va) (G.stream ub)
 {-# INLINE zips #-}
-
--- * Debugging
-
-shape :: Map k v -> [Int]
-shape Nil           = []
-shape (One _ _ m)   = 1 : shape m
-shape (Map n _ _ m) = n : shape m
