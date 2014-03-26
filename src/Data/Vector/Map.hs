f4e7822ff7d6f9c14e5a23d545650364d5f3b50d
Edward Kmett <ekmett@gmail.com>
2014-03-25 21:54:28 -0400

    refactored to use IO evilness rather than Data.Vector.Slow

--- src/Data/Vector/Map.hs
+++ src/Data/Vector/Map.hs
@@ -12,9 +12,12 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE PatternGuards #-}
+#if __GLASGOW_HASKELL__ >= 708
+{-# LANGUAGE RoleAnnotations #-}
+#endif
 -----------------------------------------------------------------------------
 -- |
--- Copyright   :  (C) 2013 Edward Kmett
+-- Copyright   :  (C) 2013-2014 Edward Kmett
 -- License     :  BSD-style (see the file LICENSE)
 -- Maintainer  :  Edward Kmett <ekmett@gmail.com>
 -- Stability   :  experimental
@@ -50,15 +53,13 @@ module Data.Vector.Map
   , fromList
   ) where
 
-import Control.Monad.ST.Unsafe as Unsafe
-import Control.Monad.ST.Class
-import Control.Monad.Trans.Iter
+import Control.Applicative hiding (empty)
 import Data.Bits
 import qualified Data.List as List
 import Data.Vector.Array
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Generic.Mutable as GM
-import Data.Vector.Slow as Slow
+import GHC.Prim (RealWorld)
 import Prelude hiding (null, lookup)
 import System.IO.Unsafe as Unsafe
 
@@ -68,19 +69,42 @@ import System.IO.Unsafe as Unsafe
 -- that are equal to those of a B-Tree, except for an extra log factor slowdown on lookups due to the lack of fractional
 -- cascading.
 
-data Chunk k a = Chunk !(Array k) !(Array a)
-
 data Map k a
   = M0
   | M1 !(Array k) !(Array a)
   | M2 !(Array k) !(Array a)
-       !(Array k) !(Array a) !(Partial (Chunk k a)) !(Map k a)
+       !(Array k) !(Array a)
+       {-# UNPACK #-} !Int {-# UNPACK #-} !Int
+       !(MArray RealWorld k) !(MArray RealWorld a)
+       !(Map k a)
   | M3 !(Array k) !(Array a)
        !(Array k) !(Array a)
-       !(Array k) !(Array a) !(Partial (Chunk k a)) !(Map k a)
+       !(Array k) !(Array a)
+       {-# UNPACK #-} !Int {-# UNPACK #-} !Int
+       !(MArray RealWorld k) !(MArray RealWorld a)
+       !(Map k a)
 
-deriving instance (Show (Arr v v), Show (Arr k k)) => Show (Map k v)
-deriving instance (Show (Arr v v), Show (Arr k k)) => Show (Chunk k v)
+#if __GLASGOW_HASKELL__ >= 708
+type role Map nominal nominal
+#endif
+
+instance (Show (Arr v v), Show (Arr k k)) => Show (Map k v) where
+  showsPrec _ M0 = showString "M0"
+  showsPrec d (M1 ka a) = showParen (d > 10) $
+    showString "M1 " . showsPrec 11 ka . showChar ' ' . showsPrec 11 a
+  showsPrec d (M2 ka a kb b ra rb _ _ xs) = showParen (d > 10) $
+    showString "M2 " .
+    showsPrec 11 ka . showChar ' ' . showsPrec 11 a . showChar ' ' .
+    showsPrec 11 kb . showChar ' ' . showsPrec 11 b . showChar ' ' .
+    showsPrec 11 ra . showChar ' ' . showsPrec 11 rb . showString " _ _ " .
+    showsPrec 11 xs
+  showsPrec d (M3 ka a kb b kc c rb rc _ _ xs) = showParen (d > 10) $
+    showString "M3 " .
+    showsPrec 11 ka . showChar ' ' . showsPrec 11 a . showChar ' ' .
+    showsPrec 11 kb . showChar ' ' . showsPrec 11 b . showChar ' ' .
+    showsPrec 11 kc . showChar ' ' . showsPrec 11 c . showChar ' ' .
+    showsPrec 11 rb . showChar ' ' . showsPrec 11 rc . showString " _ _ " .
+    showsPrec 11 xs
 
 -- | /O(1)/. Identify if a 'Map' is the 'empty' 'Map'.
 null :: Map k v -> Bool
@@ -103,9 +127,9 @@ lookup :: (Ord k, Arrayed k, Arrayed v) => k -> Map k v -> Maybe v
 lookup !k m0 = go m0 where
   {-# INLINE go #-}
   go M0 = Nothing
-  go (M1 ka va)                 = lookup1 k ka va Nothing
-  go (M2 ka va kb vb _ m)       = lookup1 k ka va $ lookup1 k kb vb $ go m
-  go (M3 ka va kb vb kc vc _ m) = lookup1 k ka va $ lookup1 k kb vb $ lookup1 k kc vc $ go m
+  go (M1 ka va)                       = lookup1 k ka va Nothing
+  go (M2 ka va kb vb _ _ _ _ m)       = lookup1 k ka va $ lookup1 k kb vb $ go m
+  go (M3 ka va kb vb kc vc _ _ _ _ m) = lookup1 k ka va $ lookup1 k kb vb $ lookup1 k kc vc $ go m
 {-# INLINE lookup #-}
 
 lookup1 :: (Ord k, Arrayed k, Arrayed v) => k -> Array k -> Array v -> Maybe v -> Maybe v
@@ -117,104 +141,66 @@ lookup1 k ks vs r
 
 -- | O((log N)\/B) worst-case loads for each cache. Insert an element.
 insert :: (Ord k, Arrayed k, Arrayed v) => k -> v -> Map k v -> Map k v
-insert k0 v0 s0 = go (G.singleton k0) (G.singleton v0) s0 where
-  go ka a M0                    = M1 ka a
-  go ka a (M1 kb b)             = M2 ka a kb b (merge ka a kb b) M0
-  go ka a (M2 kb b kc c mbc xs) = M3 ka a kb b kc c (step mbc) (steps xs)
-  go ka a (M3 kb b _ _ _ _ mcd xs) = case mcd of
-    Stop (Chunk kcd cd) -> M2 ka a kb b (merge ka a kb b) (go kcd cd xs)
-    _       -> error "insert: stop Step"
+insert k0 v0 s0 = unsafeDupablePerformIO $ go (G.singleton k0) (G.singleton v0) s0 where
+  go ka a M0        = return $ M1 ka a
+  go ka a (M1 kb b) = do
+    let n = G.length ka + G.length kb
+    kab <- GM.basicUnsafeNew n
+    ab  <- GM.basicUnsafeNew n
+    (ra,rb) <- step ka a kb b 0 0 kab ab
+    return $ M2 ka a kb b ra rb kab ab M0
+  go ka a (M2 kb b kc c rb rc kbc bc xs) = do
+    (rb',rc') <- step kb b kc c rb rc kbc bc
+    M3 ka a kb b kc c rb' rc' kbc bc <$> steps xs
+  go ka a (M3 kb b _ _ _ _ _ _ kcd cd xs) = do
+    let n = G.length ka + G.length kb
+    kab <- GM.basicUnsafeNew n
+    ab  <- GM.basicUnsafeNew n
+    (ra,rb) <- step ka a kb b 0 0 kab ab
+    kcd' <- G.unsafeFreeze kcd
+    cd' <- G.unsafeFreeze cd
+    M2 ka a kb b ra rb kab ab <$> go kcd' cd' xs
 
-  steps (M2 kx x ky y mxy xs)   = M2 kx x ky y (step mxy) (steps xs)
-  steps (M3 kx x ky y kz z myz xs) = M3 kx x ky y kz z (step myz) (steps xs)
-  steps m = m
+  steps (M2 kx x ky y rx ry kxy xy xs) = do
+    (rx',ry') <- step kx x ky y rx ry kxy xy
+    M2 kx x ky y rx' ry' kxy xy <$> steps xs
+  steps (M3 kx x ky y kz z ry rz kyz yz xs) = do
+    (ry',rz') <- step ky y kz z ry rz kyz yz
+    M3 kx x ky y kz z ry' rz' kyz yz <$> steps xs
+  steps m = return m
 {-# INLINE insert #-}
 
-step :: Partial a -> Partial a
-step (Stop _)  = error "insert: step Stop"
-step (Step m) = m
-{-# INLINE step #-}
-
-merge :: forall k a. (Ord k, Arrayed k, Arrayed a) => Array k -> Array a -> Array k -> Array a -> Partial (Chunk k a)
-merge ka va kb vb = step (walkDupableST mergeST) where
- mergeST :: forall s. IterST s (Chunk k a)
- mergeST = do
-  let
-    !la = G.length ka
-    !lb = G.length kb
-  kc <- liftST $ GM.unsafeNew (la + lb)
-  vc <- liftST $ GM.unsafeNew (la + lb)
-  let
-    goL :: Int -> Int -> IterST s ()
-    goL !i !j -- left exhausted
-      | j >= lb = return ()
-      | !k <- i + j = do
-        liftST $ do
-          kj <- G.unsafeIndexM kb j
-          GM.unsafeWrite kc k kj
-          vj <- G.unsafeIndexM vb j
-          GM.unsafeWrite vc k vj
-        delay $ goL i (j+1)
-
-    goR :: Int -> Int -> IterST s ()
-    goR !i !j -- right exhausted
-      | i >= la = return ()
-      | !k <- i + j = do
-        liftST $ do
-          ki <- G.unsafeIndexM ka i
-          GM.unsafeWrite kc k ki
-          vi <- G.unsafeIndexM va i
-          GM.unsafeWrite vc k vi
-        delay $ goR (i+1) j
-
-    go :: Int -> Int -> IterST s ()
-    go !i !j
-      | i >= la = goL i j
-      | j >= lb = goR i j
-      | !k <- i + j = do
-        ki <- liftST $ G.unsafeIndexM ka i
-        kj <- liftST $ G.unsafeIndexM kb j
-        case compare ki kj of
-          LT -> do
-            liftST $ do
-              vi <- G.unsafeIndexM va i
-              GM.unsafeWrite kc k ki
-              GM.unsafeWrite vc k vi
-            delay $ go (i+1) j
-          EQ -> do
-            liftST $ do
-              vi <- G.unsafeIndexM va i
-              GM.unsafeWrite kc k ki
-              GM.unsafeWrite vc k vi
-            delay $ go (i+1) (i+j)
-          GT -> do
-            liftST $ do
-              vj <- G.unsafeIndexM vb j
-              GM.unsafeWrite kc k kj
-              GM.unsafeWrite vc k vj
-            delay $ go i (j+1)
-  go 0 0
-  liftST $ do
-    fkc <- G.unsafeFreeze kc
-    fvc <- G.unsafeFreeze vc
-    return $ Chunk fkc fvc
-{-# INLINE merge #-}
-
-walkDupableST :: (forall s. IterST s a) -> Partial a
-walkDupableST m0 = go m0 where
-  go (IterT m) =
-    case Unsafe.unsafeDupablePerformIO $
-         Unsafe.unsafeSTToIO m of
-      Left  a -> Stop a
-      Right n -> Step (go n)
-
-{-
-merge :: (Ord k, Arrayed k, Arrayed a) => Array k -> Array a -> Array k -> Array a -> Partial (Chunk k a)
-merge km m kn n = step $ walkDupableST $ do
-  V_Pair _ ks vs <- Slow.unstreamM $ Slow.streamST $ Fusion.merge (zips km m) (zips kn n)
-  return $ Chunk ks vs
-{-# INLINE merge #-}
--}
+step :: (Ord k, Arrayed k, Arrayed v) => Array k -> Array v -> Array k -> Array v -> Int -> Int -> MArray RealWorld k -> MArray RealWorld v -> IO (Int, Int)
+step ka a kb b ra rb kab ab
+  | ra == na = do
+    k <- G.basicUnsafeIndexM kb rb
+    v <- G.basicUnsafeIndexM b rb
+    ret k v ra (rb + 1)
+  | rb == nb = do
+    k <- G.basicUnsafeIndexM ka ra
+    v <- G.basicUnsafeIndexM a ra
+    ret k v (ra + 1) rb
+  | otherwise = do
+    k1 <- G.basicUnsafeIndexM ka ra
+    k2 <- G.basicUnsafeIndexM kb rb
+    case compare k1 k2 of
+      LT -> do
+        v <- G.basicUnsafeIndexM a ra
+        ret k1 v (ra + 1) rb
+      EQ -> do -- collision, overwrite with newer value
+        v <- G.basicUnsafeIndexM a ra
+        ret k1 v (ra + 1) (rb + 1)
+      GT -> do
+        v <- G.basicUnsafeIndexM b rb
+        ret k2 v ra (rb + 1)
+  where
+    na = G.length ka
+    nb = G.length kb
+    r = ra + rb
+    ret k v !ra' !rb' = do
+      GM.basicUnsafeWrite kab r k
+      GM.basicUnsafeWrite ab r v
+      return (ra',rb')
 
 fromList :: (Ord k, Arrayed k, Arrayed v) => [(k,v)] -> Map k v
 fromList xs = List.foldl' (\m (k,v) -> insert k v m) empty xs
@@ -232,9 +218,3 @@ search p = go where
     where hml = h - l
           m = l + unsafeShiftR hml 1 + unsafeShiftR hml 6
 {-# INLINE search #-}
-
-{-
-zips :: (G.Vector v a, G.Vector u b) => v a -> u b -> Stream Id (a, b)
-zips va ub = Stream.zip (G.stream va) (G.stream ub)
-{-# INLINE zips #-}
--}
